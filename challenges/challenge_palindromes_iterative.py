6ef6fe0066743c8df4fcc7dcf7c9dbf7ac45905c
h3zord <lucaschmain@gmail.com>
2023-03-26 19:58:07 -0300

    Completing requirement 06

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    n = len(word) - 1
+
+    for index in range(0, len(word)):
+        if word[index] != word[n - index]:
+            return False
+
+    return True
