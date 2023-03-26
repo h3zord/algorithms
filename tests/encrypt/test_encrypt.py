1a7242d90629b54c4937dbdb16ef4539ee0aa16f
h3zord <lucaschmain@gmail.com>
2023-03-26 00:25:05 -0300

    Completing requirement 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("abcdef", -10) == "fedcba"
+    assert encrypt_message("abcdef", 3) == "cba_fed"
+    assert encrypt_message("abcdef", 4) == "fe_dcba"
+    assert encrypt_message("cba", 1) == "c_ab"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("test", "test")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(2, 2)
+    
