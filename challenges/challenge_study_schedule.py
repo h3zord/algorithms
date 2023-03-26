92871c52feb95dbf2f5679684f6df6887278c8f6
h3zord <lucaschmain@gmail.com>
2023-03-25 23:00:02 -0300

    Completing requirement 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (not target_time):
+        return None
+
+    time_list = []
+    for time1, time2 in permanence_period:
+        if (not str(time1) or not str(time2) or type(time1) != int or type(time2) != int):
+            return None
+        
+        time_list.extend(list(range(time1, time2 + 1)))
+
+    return time_list.count(target_time)
+
